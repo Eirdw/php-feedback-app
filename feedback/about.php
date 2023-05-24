9ddae543f47815e24f6a3d53f1840d4937f2e826
Collins Oche Aghughu <collinsoche@gmail.com>
2023-05-24 15:21:58 +0100

    first commit

---
+++ feedback/about.php
@@ -0,0 +1,9 @@
+<?php include 'inc/header.php'; ?>
+    <h2>About</h2>
+
+    <p class="text-center">Welcome to Ay exchange, the premier currency exchange platform owned by Oseni Toheeb Ayomide. We are based in Istanbul and specialize in exchanging foreign currencies, with a primary focus on Turkish Lira to Naira and Dollar to Naira conversions, among others. Our goal is to provide a seamless and reliable solution for individuals and businesses looking to exchange currencies quickly and conveniently.
+
+      At Ay exchange, we understand the importance of secure transactions, competitive rates, and excellent customer service. Our dedicated team of experts works tirelessly to ensure that your currency exchange needs are met with professionalism and efficiency. With our user-friendly app, you can easily access real-time exchange rates, place orders, and track your transactions.
+      
+      We are committed to transparency and strive to provide accurate and up-to-date information on exchange rates</p>
+<?php include 'inc/footer.php'; ?>     
